ee6057ab5b38aff74ce5a9b189f6d3f58dcb309d
Ryan Goderis <rgoderis789@gmail.com>
2019-09-25 20:48:55 -0400

    set function to check bandsintown and retrieve information to log venue city and date

--- liri.js
+++ liri.js
@@ -11,10 +11,12 @@ var fs = require("fs");
 var whatToDo = process.argv[2];
 var userInput = process.argv[3];
 
+// function to retrieve information from spotify
 function spotifyThis(input){
     spotify
         .search({ type: 'track', query: input })
         .then(function(response) {
+            // loops through spotify songs to display information
             for(var i = 0; i < response.tracks.items.length; i++){
                 // name of song
                 console.log("Song Name: "+JSON.stringify(response.tracks.items[i].name, null, 2));
@@ -30,9 +32,28 @@ function spotifyThis(input){
             console.log(err);
         });
 }
-function concertThis(){
 
+// function to retrieve information from bandsintown
+function concertThis(input){
+    // axios call to bandsintown
+    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then(
+    function(response){
+        // console.log(response.data[0].offers)
+        // display name of venue
+        console.log(response.data[0].venue.name)
+        // venue location
+        console.log(response.data[0].venue.city + ", "+ response.data[0].venue.region + ". " + response.data[0].venue.country)
+        // date of event
+        console.log(response.data[0].datetime)
+    })
+    // check for error
+    .catch(function(err){
+        if(err){
+            console.log(err)
+        }
+    })
 }
+
 function movieThis(){
 
 }
@@ -42,8 +63,10 @@ function doWhatItSays(){
 
 switch(whatToDo){
     case "spotify-this-song":
+        // checks to see if the user enters a song name
         if(process.argv.length === 3){
             spotify
+            // displays default song info of The Sign by Ace of Base
             .search({type: 'track', query: 'The Sign'})
             .then(function(response) {
                 // name of song
@@ -66,10 +89,32 @@ switch(whatToDo){
         movieThis();
         break;
     case "concert-this":
-        concertThis();
+        concertThis(userInput);
         break;
     case "do-what-it-says":
         doWhatItSays();
         break;
 }
 
+
+// { offers:
+//    [ { type: 'Tickets',
+//        url:
+//         'https://www.bandsintown.com/t/1016572846?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket',
+//        status: 'available' } ],
+//   venue:
+//    { name: 'North Island Credit Union Ampitheatre',
+//      country: 'United States',
+//      region: 'CA',
+//      city: 'Chula Vista',
+//      latitude: '32.589367',
+//      longitude: '-117.00653' },
+//   datetime: '2019-10-10T19:00:00',
+//   on_sale_datetime: '2019-08-09T10:00:00',
+//   description:
+//    'Rain or shine. Every ticket includes parking in the unpaved lots. Upgrades are available for premier parking in the paved lots and closer to the venue entrance. Parking Gates open at 4PM on day of show.',
+//   lineup: [ 'Bush' ],
+//   id: '1016572846',
+//   artist_id: '680',
+//   url:
+//    'https://www.bandsintown.com/e/1016572846?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event' }
